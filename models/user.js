0826a7b290f9f960329501aae1d1aa6bc8aef896
Unknown <wardm89@yahoo.com>
2017-09-19 17:19:10 -0400

    VERY simple registration system.
    Has to use a REST api to send dummy json object and replys success in the console.log()

---
+++ models/user.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Matt's Beast PC on 9/19/2017.
+ */
+
+const mongoose      = require('mongoose');
+const bcrypt        = require('bcryptjs');
+const config        = require('../config/database');
+
+const UserSchema    = mongoose.Schema({
+    name: {
+       type: String
+   },
+    email: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById  = function (id, callback) {
+    User.findById(id, callback);
+};
+
+module.exports.getUserByUsername  = function (username, callback) {
+    const query = {username: username};
+    User.findOne(query, callback);
+};
+
+module.exports.addUser  = function (newUser, callback) {
+
+  bcrypt.genSalt(10, (err, salt) => {
+      if(err) throw err;
+     bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if(err) throw err;
+         newUser.password   = hash;
+         newUser.save(callback);
+     });
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,40 @@
 /**
  * Created by Matt's Beast PC on 9/19/2017.
  */
+
+
 const express       = require('express');
 const router        = express.Router();
+const User          = require('../models/user');
+const jwt           = require('jsonwebtoken');
 
-router.get('/register', (request, response, next) => {
-    response.send('Register');
+router.post('/register', (request, response, next) => {
+    let newUser = new User({
+        name:       request.body.name,
+        email:      request.body.email,
+        username:   request.body.username,
+        password:   request.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err)
+        {
+            response.json({success: false, msg: 'Failed to register User'});
+        }
+        else
+        {
+            response.json({success: true, msg: 'User registered'});
+        }
+    });
+
 });
 
 router.post('/authenticate', (request, response, next) => {
     response.send('Authenticate');
-})
+});
 
 router.get('/profile', (request, response, next) => {
     response.send('Profile');
-});
-
-router.get('/validate', (request, response, next) => {
-    response.send('Validate');
 });
 
 module.exports  = router;
